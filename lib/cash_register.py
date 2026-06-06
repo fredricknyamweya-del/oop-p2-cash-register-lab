cd17c4110bb06c02b2488922e1078435e15e7000
Fredrick Nyamweya <fredrick.nyamweya@student.moringaschool.com>
2026-06-06 11:26:41 +0300

    Initial commit

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        if isinstance(discount, int) and 0 <= discount <= 100:
+            self.discount = discount
+        else:
+            print("Not valid discount")
+            self.discount = 0
+
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
+
+    def add_item(self, item, price, quantity=1):
+        self.total += price * quantity
+        self.items.append({"item": item, "price": price, "quantity": quantity})
+        self.previous_transactions.append({"item": item, "price": price, "quantity": quantity})
+
+    def apply_discount(self):
+        if self.discount > 0:
+            discount_amount = self.total * (self.discount / 100)
+            self.total -= discount_amount
+            print(f"Discount applied: -{discount_amount}")
+        else:
+            print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+        if self.previous_transactions:
+            last_transaction = self.previous_transactions.pop()
+            self.total -= last_transaction["price"] * last_transaction["quantity"]
+            self.items.pop()
+            print(f"Voided last transaction: {last_transaction}")
+        else:
+            print("No transaction to void.")
+
